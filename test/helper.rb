2549250eeed69aed17e0a6426cfa7acce96fcef9
Kenji Okimoto <okimoto@clear-code.com>
2016-09-14 17:25:18 +0900

    Add missing require

--- test/helper.rb
+++ test/helper.rb
@@ -2,7 +2,10 @@ require "test/unit"
 require "test/unit/rr"
 require "test/unit/notify"
 require "fluent/test"
+require "fluent/test/helpers"
 require "fluent/plugin/out_sql"
 require "fluent/plugin/in_sql"
+
+include Fluent::Test::Helpers
 
 load "fixtures/schema.rb"
